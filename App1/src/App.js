486709c13906e143a032cd41864a6290d4a5d99c
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-08-18 15:40:29 +0700

    event-data

--- App1/src/App.js
+++ App1/src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Header, Content, Footer } from './func-components'
 import Calendar from './class-components'
 import Banner from './banner'
-
+import {Calculator} from './calculator'
+import { Calculator2 } from './calculator2'
+import {Table} from './event-data'
 
 function App() {
     const divStyle={
@@ -13,13 +15,16 @@ function App() {
     }
     return ( 
         <>
-            <Banner/>
             <Header/>
+            <Banner/>
+            <Table/>
             <p><center><Calendar/></center></p>
             <Content/>
             <h3 style={divStyle}> Hello Na World 
                 <br/> สวัสดีนะจ๊ะ 
             </h3>
+            <Calculator/>
+            <Calculator2/>
             <Footer/>
         </>
     );
