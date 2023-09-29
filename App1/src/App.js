f3cbad7be13178b1a11817242d5bc2ec47f5c995
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-09-29 14:11:20 +0700

    bootstrap1st

--- App1/src/App.js
+++ App1/src/App.js
@@ -15,15 +15,15 @@ import React, { useState,useContext} from 'react'
 //import { userContext } from './context'
 //import Header2 from './context-header2'
 //import Content2 from './context-content2'
-import Router2 from './router2'
-import Router3 from './router3';
-
+//import Router2 from './router2'
+//import Router3 from './router3';
+import FormBootstrap from './form-bootstrap'
 
 
 function App() {
    
     return ( 
-        <Router3/>
+        <FormBootstrap/> 
     );
     
 }
