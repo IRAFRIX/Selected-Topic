4a99266ec7f1dcc0fae3f5d3a4de2f52f315334f
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-09-29 15:55:31 +0700

    almostFinish

--- App1/src/App.js
+++ App1/src/App.js
@@ -19,12 +19,13 @@ import React, { useState,useContext} from 'react'
 //import Router3 from './router3';
 import FormBootstrap from './form-bootstrap'
 import FormText from './form-text'
+import FormCheck from './form-check'
 
 
 function App() {
    
     return ( 
-        <FormText/> 
+        <FormCheck/> 
     );
     
 }
